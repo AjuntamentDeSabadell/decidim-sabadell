eb4d2ab7c3b7adf4987b3e2e97e928919f773683
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-02-15 11:42:16 +0100

    Set catalan to the default language

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module DecidimSabadell
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.i18n.default_locale = :ca
   end
 end
