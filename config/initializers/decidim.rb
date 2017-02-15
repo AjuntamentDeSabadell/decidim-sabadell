80461ce23a272c2d4bbcf306611a377c11e2f5e1
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-02-15 11:39:32 +0100

    Change host

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # frozen_string_literal: true
 Decidim.configure do |config|
-  config.application_name = "My Application Name"
-  config.mailer_sender    = "change-me@domain.org"
-  config.authorization_handlers = [ExampleAuthorizationHandler]
+  config.application_name = "Decidim Sabadell"
+  config.mailer_sender    = "decidim@codegram.com"
+  config.authorization_handlers = []
 
   # Uncomment this lines to set your preferred locales
   # config.available_locales = %i{en ca es}
