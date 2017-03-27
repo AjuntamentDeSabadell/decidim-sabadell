0e0cf45dd07693bfcecf1d67e9b254c22c4ef6bd
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-03-27 17:12:58 +0200

    Fix migration script

--- migrate.sh
+++ migrate.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+bundle exec rake db:migrate assets:precompile
 curl -i -u sabadell:$DOCKER_CLOUD_API_KEY -X POST -H "Accept: application/json" "https://cloud.docker.com/api/app/v1/service/$APP_SERVICE_ID/redeploy/"
