bd4b59da88575c7f80fdfdb69e2903eca0490fa5
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-03-28 10:43:49 +0200

    Fix migration scripts

--- run-app.sh
+++ run-app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./wait-for-postgres.sh
+./wait-for-postgres.sh $POSTGRES_HOST
 bundle exec rake db:create &> /dev/null
 bundle exec rake db:migrate assets:precompile && bundle exec passenger start
