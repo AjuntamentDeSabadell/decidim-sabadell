c9b7776df779d8e7da28555588dc4cf96400394d
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-03-28 10:41:18 +0200

    Wait for postgres

--- migrate.sh
+++ migrate.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+./wait-for-postgres.sh
 bundle exec rake db:migrate assets:precompile
 curl -i -u sabadell:$DOCKER_CLOUD_API_KEY -X POST -H "Accept: application/json" "https://cloud.docker.com/api/app/v1/service/$APP_SERVICE_ID/redeploy/"
--- run-app.sh
+++ run-app.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+./wait-for-postgres.sh
 bundle exec rake db:create &> /dev/null
 bundle exec rake db:migrate assets:precompile && bundle exec passenger start
