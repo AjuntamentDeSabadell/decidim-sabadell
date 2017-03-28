8e23f961d2e8a55ddf964cf8b3456d2a5dd1876e
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-03-28 09:39:48 +0200

    Add sidekiq only on production

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  authenticate :user, lambda { |u| u.roles.include?("admin") } do
-    mount Sidekiq::Web => '/sidekiq'
+  if Rails.env.production?
+    require 'sidekiq/web'
+    authenticate :user, lambda { |u| u.roles.include?("admin") } do
+      mount Sidekiq::Web => '/sidekiq'
+    end
   end
 
   mount Decidim::Core::Engine => '/'
