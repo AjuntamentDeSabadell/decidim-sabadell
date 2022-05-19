9e280a8c9ce1600db70be0a8b281b13bf1650cf1
SergioMedeirosGarcia <sergio.medeiros@sngular.com>
2022-05-19 13:19:49 +0200

    Fixing migrations errors

--- db/migrate/20220517115214_add_salt_to_decidim_forms_questionnaires.decidim_forms.rb
+++ db/migrate/20220517115214_add_salt_to_decidim_forms_questionnaires.decidim_forms.rb
@@ -10,8 +10,7 @@ class AddSaltToDecidimFormsQuestionnaires < ActiveRecord::Migration[5.2]
     add_column :decidim_forms_questionnaires, :salt, :string
 
     Questionnaire.find_each do |questionnaire|
-      custom_salt = questionnaire.id > 99 ? "3acd7793f2a8cbed2d2b73b91ebe31c#{questionnaire.id}4921f71f83009fa8494e260d54c" : questionnaire.id > 9 ? "3acd7793f2a8cbed2d2b73b91ebe431a#{questionnaire.id}4921f71f83009fa8494e260d54c" : "3acd7793f2a8cbed20d2b73b91ebe431t#{questionnaire.id}4921f71f83009fa8494e260d54c"
-      questionnaire.salt = custom_salt
+      questionnaire.salt = Decidim::Tokenizer.random_salt
       questionnaire.save!
     end
   end
